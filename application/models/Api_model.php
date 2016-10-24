69c1f6b33f622aa1f30dfd12453f4a8db6f63413
Jamie <j_r_clarke@hotmail.com>
2016-10-24 16:30:28 +0100

    Amendments to API for joining tables

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -21,6 +21,7 @@ class api_model extends CI_Model {
 	{
 		$this->db->select('*');
 		$this->db->from('tbl_sites');
+		$this->db->join('tbl_countries','tbl_countries.country_id = tbl_sites.site_country');
 		$result = $this->db->get();
 		return $result;
 	}
@@ -29,6 +30,8 @@ class api_model extends CI_Model {
 	{
 		$this->db->select('*');
 		$this->db->from('tbl_hosts');
+		$this->db->join('tbl_sites','tbl_sites.site_id = tbl_hosts.host_site');
+		$this->db->join('tbl_hosttypes','tbl_hosttypes.ht_id = tbl_hosts.host_type');
 		$result = $this->db->get();
 		return $result;
 	}
